f0352a7e6170c9d2aba8410dc722261c5a4eb240
sergd <videomc1979@gmail.com>
2023-03-29 23:58:48 +0300

    HW_30 removed duplicate code

--- users/serializers.py
+++ users/serializers.py
@@ -8,31 +8,6 @@ class LocationListSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-# class LocationDetailSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Location
-#         fields = "__all__"
-#
-#
-# class LocationCreateSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Location
-#         fields = "__all__"
-#
-#
-# class LocationUpdateSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Location
-#         fields = "__all__"
-#
-#
-# class LocationDestroySerializer(serializers.ModelSerializer):
-#     model = Location
-#     fields = [
-#         "id",
-#     ]
-
-
 class UserSerializer(serializers.ModelSerializer):
     location = LocationListSerializer(many=True)
 
