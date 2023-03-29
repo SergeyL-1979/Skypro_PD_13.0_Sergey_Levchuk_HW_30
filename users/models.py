1a2cdd1708f58a1157ffefebedcf02667ee21f1e
sergd <videomc1979@gmail.com>
2023-03-29 23:42:49 +0300

    HW_30 removed commented code and imports

--- users/models.py
+++ users/models.py
@@ -12,10 +12,7 @@ class User(AbstractUser):
         ("moderator", "Модератор"),
         ("member", "Участник"),
     ]
-    # first_name = models.CharField(_("Имя"), max_length=50)
-    # last_name = models.CharField(_("Фамилия"), max_length=50, null=True, blank=True)
-    # username = models.CharField(_("Никнейм"), max_length=50)
-    # password = models.CharField(_("password"), max_length=150)
+
     role = models.CharField(
         _("Права пользователя"), max_length=10, choices=STATUS, default="member"
     )
@@ -23,12 +20,10 @@ class User(AbstractUser):
     location = models.ManyToManyField(
         "Location", blank=True, verbose_name="Местоположение"
     )
-    # location = models.ForeignKey(Location, on_delete=models.CASCADE, verbose_name='Местоположение')
 
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
-        # ordering = ["username"]
 
     def __str__(self):
         return "{} {}".format(self.last_name, (self.username,))
@@ -38,8 +33,6 @@ class Location(models.Model):
     """Модель местоположения"""
 
     name = models.CharField(_("Местоположение"), max_length=50)
-    # lat = models.FloatField(_("lat"), unique=True)
-    # lng = models.FloatField(_("lng"), unique=True)
     lat = models.DecimalField(_("lat"), max_digits=8, decimal_places=6, null=True, blank=True)
     lng = models.DecimalField(_("lng"), max_digits=8, decimal_places=6, null=True, blank=True)
 
