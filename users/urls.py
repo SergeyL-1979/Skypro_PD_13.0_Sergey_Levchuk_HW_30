b2153b2e77b94617d3041220f15749174d70ae43
sergd <videomc1979@gmail.com>
2023-03-29 23:57:19 +0300

    HW_30 added token authorization

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,20 @@
 from django.urls import path
 
-from users import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.authtoken import views
+
+from users.views import UserListView, UserDetailView, \
+    UserCreateView, UserUpdateView, UserDeleteView, Logout
 
 urlpatterns = [
-    path("", views.UserListView.as_view(), name="users_list"),
-    path("<int:pk>/", views.UserDetailView.as_view(), name="users_detail"),
-    path("create/", views.UserCreateView.as_view(), name="users_create"),
-    path("<int:pk>/update/", views.UserUpdateView.as_view(), name="users_update"),
-    path("<int:pk>/delete/", views.UserDeleteView.as_view(), name="users_delete"),
+    path("", UserListView.as_view()),
+    path("<int:pk>/", UserDetailView.as_view()),
+    path("create/", UserCreateView.as_view()),
+    path("<int:pk>/update/", UserUpdateView.as_view()),
+    path("<int:pk>/delete/", UserDeleteView.as_view()),
+
+    path('login/', views.obtain_auth_token),
+    path('logout/', Logout.as_view()),
+    path('token/', TokenObtainPairView.as_view()),
+    path('token/refresh/', TokenRefreshView.as_view()),
 ]
